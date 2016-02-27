66d3dd306e4e82a2d0ab1799d6b1e38114d2708f
nasa <nasa.freaks@gmail.com>
2016-02-27 13:10:28 +0700

    server requests done

--- server_requests.py
+++ server_requests.py
@@ -1,11 +1,18 @@
 def processingRequests(servers, requests):
     hasil = []
+    x = []
+    result = []
     for i in range(1, servers + 1):
-        hasil.append([x for x in requests[:2*i] if x < ])
-    print hasil
-    return len(hasil)
+        process_data = range(1, (2*i)+1)
+        hasil.append([x for x in requests if x in process_data])
+    #for x in range(len(hasil)):
+    for i in range(len(hasil)):
+        if len(hasil[i]) > i:
+            result.append(hasil[i][i])
+    print result
+    return len(result)
 
 
 
 if __name__ == "__main__":
-    print processingRequests(2, [1, 5, 6])
+    print processingRequests(3, [1,2,3,4,5,6,7,8,9,10])
